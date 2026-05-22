7ca30a86ed584b5640ab2258755225b2e31dc02b
dkhanh264 <24638601.khanh@student.iuh.edu.vn>
2026-05-23 01:00:15 +0700

    install new pkgs

--- home/core/packages.nix
+++ home/core/packages.nix
@@ -21,6 +21,7 @@
     blueman
     pavucontrol
     nautilus
+    pulseaudio
     mesa-demos
     nwg-look
 
@@ -44,16 +45,6 @@
     vscode
     fastfetch
 
-    # Thêm các công cụ từ nixparency-dots
-    walker         # Thay thế Rofi làm launcher
-    mako           # Thay thế Dunst làm notification daemon
-    swaybg         # Đặt hình nền tĩnh
-    mpvpaper       # Đặt hình nền động (video)
-    ffmpeg         # Cần cho script trích xuất ảnh từ video
-    libqalculate   # Cho module máy tính của walker
-    jq             # Cần cho các script xử lý JSON
-    libnotify
-    
   ];
 }
 
