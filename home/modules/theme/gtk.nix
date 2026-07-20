d5c90edba519aaedbee035ac0ca01cedf19a4195
dkhanh264 <24638601.khanh@student.iuh.edu.vn>
2026-07-20 21:53:39 +0700

    replace cursors and config nixvim for react native

--- home/modules/theme/gtk.nix
+++ home/modules/theme/gtk.nix
@@ -1,6 +1,26 @@
 # File: home/modules/theme/gtk.nix
 { pkgs, ... }:
 
+let
+  frierenCursor = pkgs.stdenvNoCC.mkDerivation {
+    pname = "frierenblz-cursor-theme";
+    version = "local";
+
+    src = ../../../assets/cursors/FrierenBLZ;
+
+    dontUnpack = true;
+    dontBuild = true;
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p "$out/share/icons/FrierenBLZ"
+      cp -r "$src"/. "$out/share/icons/FrierenBLZ/"
+
+      runHook postInstall
+    '';
+  };
+in
 {
   home.packages = with pkgs; [
     pywal
@@ -8,36 +28,40 @@
 
   gtk = {
     enable = true;
+
     theme = {
-      name    = "Adwaita-dark";
+      name = "Adwaita-dark";
       package = pkgs.gnome-themes-extra;
     };
+
     iconTheme = {
-      name    = "Papirus-Dark";
+      name = "Papirus-Dark";
       package = pkgs.papirus-icon-theme;
     };
-    cursorTheme = {
-      name    = "Adwaita";
-      size    = 24;
-      package = pkgs.adwaita-icon-theme;
+
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = true;
     };
-    gtk3.extraConfig.gtk-application-prefer-dark-theme = true;
-    gtk4.extraConfig.gtk-application-prefer-dark-theme = true;
+
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = true;
+    };
   };
 
   home.pointerCursor = {
-    name       = "Adwaita";
-    size       = 24;
-    package    = pkgs.adwaita-icon-theme;
+    enable = true;
+
+    name = "FrierenBLZ";
+    package = frierenCursor;
+    size = 24;
+
     gtk.enable = true;
     x11.enable = true;
   };
 
   qt = {
-    enable             = true;
+    enable = true;
     platformTheme.name = "gtk";
-    style.name         = "adwaita-dark";
+    style.name = "adwaita-dark";
   };
-  
-  # Đã xóa bỏ hoàn toàn phần xdg.configFile import file css bị lỗi
 }
