850cfc3a0856031e09205d8c9338bb2e4f63262e
dkhanh264 <24638601.khanh@student.iuh.edu.vn>
2026-08-15 17:37:18 +0700

    optimize (3)

--- home/modules/terminal/btop.nix
+++ home/modules/terminal/btop.nix
@@ -1,7 +1,8 @@
-_:
+{ pkgs, ... }:
 {
   programs.btop = {
     enable = true;
+    package = pkgs.btop.override { cudaSupport = true; };
     settings = {
       # wal-color-export maintains ~/.config/btop/themes/wal.theme.
       color_theme = "wal";
@@ -9,6 +10,13 @@ _:
       truecolor = true;
       graph_symbol = "braille";
       update_ms = 1000;
+      shown_boxes = "cpu mem net proc gpu0";
+      show_gpu_info = "Auto";
+      selected_gpu = "Auto";
+      show_battery = true;
+      show_battery_watts = true;
+      check_temp = true;
+      gpu_mirror_graph = true;
     };
   };
 }
