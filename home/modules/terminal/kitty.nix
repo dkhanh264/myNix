e924cbbec5d95d41dc07d4dd555a957a20378a36
dkhanh264 <24638601.khanh@student.iuh.edu.vn>
2026-08-31 19:24:00 +0700

    serpantinum (9)

--- home/modules/terminal/kitty.nix
+++ home/modules/terminal/kitty.nix
@@ -1,8 +1,28 @@
-{ serpantinum, ... }:
+{ lib, serpantinum, ... }:
+let
+  serpantinumKittyConf = builtins.readFile "${serpantinum}/config/kitty/kitty.conf";
+  customizedKittyConf = lib.replaceStrings
+    [
+      "background_opacity 1.0"
+      "font_size        16.0"
+    ]
+    [
+      ''
+        # Frosted glass blur effect
+        background_opacity 0.56
+        dynamic_background_opacity yes
+        background_blur 1''
+      "font_size        12.0"
+    ]
+    serpantinumKittyConf;
+in
 {
   programs.kitty = {
     enable = true;
-    extraConfig = builtins.readFile "${serpantinum}/config/kitty/kitty.conf";
+    extraConfig = customizedKittyConf;
   };
 }
+
+
+
 
